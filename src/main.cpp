46a0d58d7f1815c0b21bf6c4ba1f0bfb7ddcbda2
frazoni <diogofrazao.work@gmail.com>
2025-09-06 18:04:00 +0100

    Import and link SDL3

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,24 @@
-#include <iostream>
+#include <SDL3/SDL.h>
 #include "log.h"
 
-int main()
+int main(int argc, char* argv[])
 {
-    D_LOG(MINI, "Test");
-    D_LOG(LOG, "Test");
-    D_LOG(WARNING, "Test");
-    D_LOG(ERROR, "Test");
-    return 0;
+	if (!SDL_Init(SDL_INIT_VIDEO)) 
+	{
+		D_ASSERT(false, "Failed to init SDL: %s", SDL_GetError());
+		return 1;
+	}
+
+	SDL_Window* window = SDL_CreateWindow("Test", 960, 540, SDL_WINDOW_RESIZABLE);
+	if (!window) 
+	{
+		D_ASSERT(false, "Failed to create window: %s", SDL_GetError());
+		return 1;
+	}
+
+	SDL_Delay(5000);
+	SDL_DestroyWindow(window);
+	SDL_Quit();
+
+	return 0;
 }
